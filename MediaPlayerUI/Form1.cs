c7b5939032834f41d3701d2fc121c5f87d0a9779
gerardroyo <gerard.royo@mataro.epiaedu.cat>
2020-10-30 16:08:36 +0100

    MenuMedia and SubMenu created, can show and hide the submenu

--- MediaPlayerUI/Form1.cs
+++ MediaPlayerUI/Form1.cs
@@ -15,6 +15,74 @@ namespace MediaPlayerUI
         public Form1()
         {
             InitializeComponent();
+            customerDesign();
+        }
+        private void customerDesign()
+        {
+            panelMediaSubMenu.Visible = false;
+            panelPlaylistSubmenu.Visible = false;
+            panelToolsSubMenu.Visible = false;
+            //..
+        }
+
+        private void hideSubMenu()
+        {
+            if (panelMediaSubMenu.Visible)
+            panelMediaSubMenu.Visible = false;
+
+            if (panelPlaylistSubmenu.Visible)
+                panelPlaylistSubmenu.Visible = false;
+
+            if (panelToolsSubMenu.Visible)
+                panelToolsSubMenu.Visible = false;
+        }
+
+        private void showSubMenu(Panel subMenu)
+        {
+            if (!subMenu.Visible)
+            {
+                hideSubMenu();
+                subMenu.Visible = true;
+            }
+            else
+                subMenu.Visible = false;
+        }
+
+        private void btnMedia_Click(object sender, EventArgs e)
+        {
+            showSubMenu(panelMediaSubMenu);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //..
+            // Code
+            //..
+            hideSubMenu();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //..
+            // Code
+            //..
+            hideSubMenu();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            //..
+            // Code
+            //..
+            hideSubMenu();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            //..
+            // Code
+            //..
+            hideSubMenu();
         }
     }
 }
